5285c7d3f8250e5919a62ab65b64d277a10c46bd
ana-cegarra <ana.cegarra@gmail.com>
2024-10-30 11:40:15 -0300

    Create style.css

---
+++ css/style.css
@@ -0,0 +1,21 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 50px;
+}
+
+textarea {
+    width: 80%;
+    height: 100px;
+    margin: 10px 0;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+}
+
+#result {
+    margin-top: 20px;
+    font-weight: bold;
+}
